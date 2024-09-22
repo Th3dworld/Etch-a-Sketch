66b5281c16853b43440ef0d29433afb15f341c93
Matanda Phiri <matandaphiri64@gmail.com>
2024-09-21 21:27:13 -0500

    Make javascript functional

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 //Set up variables
 const container = document.getElementById("container");
-const gridSquare = document.createElement("div");
-
-//set gridSquareAttributes
-gridSquare.setAttribute("style", "height: 30px; width: 30px; border: orange solid 1px; margin: 0px");
 
 for(let i = 0; i < (16*16); i++){
     container.innerHTML += `<div class="gridSquare"></div>`;
 }
+
+const gridSquares = document.querySelectorAll('.gridSquare');
+
+gridSquares.forEach(grid => {
+    grid.addEventListener("mouseover", () => {
+        grid.setAttribute("style", "background-color:yellow;")
+    });
+    grid.addEventListener('mouseleave', () => {
+        setTimeout(() => {
+            grid.setAttribute("style", `background-color:white;`)
+        },
+        250);
+
+    });
+});
