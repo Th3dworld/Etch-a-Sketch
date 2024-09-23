c3f609b6d0e0874dcb6659b080b9b54d00c32249
Matanda Phiri <matandaphiri64@gmail.com>
2024-09-22 23:07:09 -0500

    Make javascript functional

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
 //Set up variables
 const container = document.getElementById("container");
+const changeGridBtn = document.getElementById("sizer");
+const divLine =  document.createElement("div");
 
-for(let i = 0; i < (16*16); i++){
-    container.innerHTML += `<div class="gridSquare"></div>`;
+//Add class to divLine
+divLine.setAttribute("id", "divline")
+
+//This function draws the grids
+function addGrids(gridSize){
+    //reset parent nodes
+    container.innerHTML = "";
+    divLine.innerHTML = "";
+
+    for(let i = 0; i < dimensions; i++){
+        divLine.innerHTML += `<div class="gridSquare"></div>`;
+    }
+    for(let i = 0; i < dimensions; i++){
+        container.appendChild(divLine.cloneNode(true));
+    }
+
+    const gridSquares = document.querySelectorAll('.gridSquare');
+
+
+    gridSquares.forEach(grid => {
+        grid.addEventListener("mouseover", () => {
+            grid.setAttribute("style", "background-color:#30F2F2;")
+        });
+        grid.addEventListener('mouseleave', () => {
+            setTimeout(() => {
+                grid.setAttribute("style", `background-color:white;`)
+            },
+            250);
+    
+        });
+    });
 }
 
-const gridSquares = document.querySelectorAll('.gridSquare');
 
-gridSquares.forEach(grid => {
-    grid.addEventListener("mouseover", () => {
-        grid.setAttribute("style", "background-color:yellow;")
-    });
-    grid.addEventListener('mouseleave', () => {
-        setTimeout(() => {
-            grid.setAttribute("style", `background-color:white;`)
-        },
-        250);
-
-    });
+let dimensions = 16;//Variable used to set dimensions initialized at 16
+ 
+changeGridBtn.addEventListener("click", ()=>{
+    dimensions = parseInt(prompt("How many squared do you want per side?"))
+    addGrids(dimensions);
 });
+
+//Change button color on click
+changeGridBtn.addEventListener("mousedown", () =>{
+    changeGridBtn.setAttribute("style", "background-color:white;")
+});
+
+changeGridBtn.addEventListener("mouseup", () =>{
+    changeGridBtn.setAttribute("style", "background-color:#AF9AB2 ;")
+});
+
+addGrids(dimensions);
+
+
